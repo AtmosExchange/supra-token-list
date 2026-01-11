c113ae0cd9350576d4287f21ac349bae527f7f2a
onotoko <friendforever0710@gmail.com>
2026-01-11 20:45:49 +0700

    feat: add SUPDOG token request

--- token-addition-request.ts
+++ token-addition-request.ts
@@ -2,19 +2,17 @@ import { TokenRequestType } from "types"
 
 const requestList: TokenRequestType[] = []
 
-// Add the details of the token you want to add to the list like this:
-// const tokenRequest: TokenRequestType = {
-//     coinAddress: null,
-//     faAddress: "0x1234567890123456789012345678901234567890",
-//     name: "Token Name",
-//     symbol: "TOKEN",
-//     decimals: 18,
-//     logoUrl: "https://raw.githubusercontent.com/AtmosExchange/supra-token-list/main/logos/FILE_NAME.FILE_EXTENSION",
-//     projectUrl: "https://example.com",
-//     twitterUrl: "https://example.com/twitter",
-//     telegramUrl: "https://example.com/telegram",
-//     discordUrl: "https://example.com/discord",
-//     coinGeckoId: "token-gecko-id",
-//     coinMarketCapId: 1234567890,
-// }
-// and open a PR with the changes
+const tokenRequest: TokenRequestType = {
+    coinAddress: null,
+    faAddress: "0x2f2514cbada3f20f092cbf6aa19ccba08c952002c1d05692154286d55682cdd1::SUPDOG::SUPDOG",
+    name: "SUPDOG",
+    symbol: "SUPDOG",
+    decimals: 6,
+    logoUrl: "https://pbs.twimg.com/media/G0FDPIgWUAAeuG6?format=jpg&name=small",
+    projectUrl: "https://www.supdog.fun/",
+    twitterUrl: "https://x.com/supdogonsupra",
+    telegramUrl: "https://t.co/G4NhcGVmjO",
+    discordUrl: null,
+    coinGeckoId: null,
+    coinMarketCapId: null,
+}
